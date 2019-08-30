284e8a8783b3e1bd95f361974ca259dda3ad3acd
Stefan Simik <stefan.simik@gmail.com>
2019-08-30 22:57:36 +0200

    Speed typing: splitted into (python & pandas).

--- speedup_typing/pandas.py
+++ speedup_typing/pandas.py
@@ -2,35 +2,21 @@ import pandas as pd
 
 df, df1, df2 = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
 
-
-# ----------------------------------------
-# PYTHON
-# ----------------------------------------
-
-my_list = ['a', 'b', 'c', 'd']
-
-my_dict1 = {'k1': 'v1', 'k2': 'v2', 'k3': 'v3'}
-
-my_dict2 = {'k1': 'v1',
-            'k2': 'v2',
-            'k3': 'v3'}
-
-
 # ----------------------------------------
 # PANDAS
 # ----------------------------------------
 
 df.apply(lambda row: row['col'], axis=1)
 
+df.drop(columns=['c1', 'c2'], inplace=True)
+
 pd.merge(df1, df2, left_on='a', right_on='b', how='left')
 pd.merge(df1, df2, left_on=['a1', 'b1'], right_on=['a2', 'b2'], how='left')
 
 df.pivot_table(index=['c1', 'c2'], columns=['c3', 'c4'], aggfunc={'c3': 'count', 'c4': sum})
 
-df.reset_index(inplace=True)
-
 df.rename(columns={'c1': 'c2', 'c3': 'c4', 'c5': 'c6'}, inplace=True)
 
-df.sort_values(['c1', 'c2', 'c3'], ascending=[True, True, True], inplace=True)
+df.reset_index(inplace=True)
 
-df.drop(columns=['c1', 'c2'], inplace=True)
+df.sort_values(['c1', 'c2', 'c3'], ascending=[True, True, True], inplace=True)
